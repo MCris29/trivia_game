c6ba6ae86b8bfd43bb29e221e58fd1579992e481
MCris29 <crismax0629@gmail.com>
2021-09-08 00:40:47 -0500

    error message

--- lib/src/pages/error.dart
+++ lib/src/pages/error.dart
@@ -3,49 +3,49 @@ import 'package:flutter/material.dart';
 class ErrorPage extends StatelessWidget {
   final String message;
 
-  const ErrorPage({Key key, this.message = "There was an unknown error." }) : super(key: key);
-
+  const ErrorPage({Key key, this.message = "Ocurrió un error desconocido."})
+      : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Error'),
         elevation: 0,
+        backgroundColor: Colors.black87,
       ),
       body: Container(
         height: double.infinity,
         width: double.infinity,
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Theme.of(context).primaryColor,
-              Theme.of(context).accentColor
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter
-          )
+          color: Colors.black87,
         ),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Center(
             child: Card(
+              color: Colors.grey.shade800,
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(message,textAlign: TextAlign.center,style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.red
-                    ),),
-                    SizedBox(height: 20.0),
-                    RaisedButton(
-                      child: Text("Try Again"),
-                      onPressed: ()=> Navigator.pop(context),
-                    )
-                  ],
+                child: Container(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Text(
+                        message,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white),
+                      ),
+                      SizedBox(height: 20.0),
+                      RaisedButton(
+                        color: Colors.green,
+                        child: Text("Intentar de nuevo"),
+                        onPressed: () => Navigator.pop(context),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
